e6080d9354d7d4483bb4958894aac1ad169abb65
dimnov <dimnov1512@gmail.com>
2024-07-13 18:13:03 +0300

    add dynamic result data

--- src/components/ResultTable.jsx
+++ src/components/ResultTable.jsx
@@ -1,5 +1,27 @@
 function ResultTable({ result }) {
   return (
+    <table className="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Total Savings</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {result.map((res) => (
+          <tr key={res.year}>
+            <td>{res.year}</td>
+            <td>{res.savingsEndOfYear}</td>
+            <td>{res.yearlyInterest}</td>
+            <td>{res.yearlyContribution}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 }
+
 export default ResultTable;
